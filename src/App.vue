953a0feff5b5a5cfeafe654edaacdb40fff7e113
Cesar <cesar_florespal@outlook.com>
2020-07-24 14:55:36 -0600

    [FL-1] Proper component separation

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,13 @@
     <div id="app" class="text-left" style="height: 100%">
         <b-container fluid style="height: 100%">
             <b-row style="height: 100%">
-                <b-col v-bind:style="[disp? {display:''}:{display:'none'}]" class="sideBar p-0">
-                    <div class="blackBlur">
-                        <div class="noBlackBlur p-3">
-                            <b-list-group-item class="mb-2 bg-transparent border-0 mb-2 rounded text-white"><img src="./assets/logo.png" alt="logo" height="60" width="60" />CesarFloresPraim</b-list-group-item>
-                            <hr class="separator">
-                            <b-list-group>
-                                <b-list-group-item v-on:click="colour(0)" v-bind:class="[arrTabs[0]? 'selectedTab':'bg-transparent']" class="rounded border-0 text-white" href="#some-link"><b-icon icon="columns" class="mr-2"></b-icon>Dashboard</b-list-group-item>
-                                <b-list-group-item v-on:click="colour(1)" v-bind:class="[arrTabs[1]? 'selectedTab':'bg-transparent']" class="rounded border-0 text-white" href="#"><b-icon icon="person-fill" class="mr-2"></b-icon>User Profile</b-list-group-item>
-                                <b-list-group-item v-on:click="colour(2)" v-bind:class="[arrTabs[2]? 'selectedTab':'bg-transparent']" class="rounded border-0 text-white" href="#"><b-icon icon="graph-up" class="mr-2"></b-icon>Charts</b-list-group-item>
-                                <b-list-group-item v-on:click="colour(3)" v-bind:class="[arrTabs[3]? 'selectedTab':'bg-transparent']" class="rounded border-0 text-white" href="#foobar"><b-icon icon="envelope-fill" class="mr-2"></b-icon>Notifications</b-list-group-item>
-                            </b-list-group>
-                        </div>
-                    </div>
-                </b-col>
-
+                <SideBar :disp="disp" />
                 <b-col v-bind:class="[disp ? 'col-10' : 'col-12']" class="bg-light">
-                    <b-icon v-on:click="toggleNav" icon="list" font-scale="2"></b-icon>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab architecto consectetur cumque dignissimos eveniet id illo illum incidunt itaque, laudantium magnam minima nobis non qui quibusdam voluptatibus voluptatum. Dolores, eius.lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquam aliquid culpa dolorem eos et exercitationem fugiat perspiciatis praesentium quam quia quos repellendus reprehenderit saepe sequi tempora, temporibus voluptate voluptatibus?</p>
+                    <b-icon v-on:click="changeSidebarProp" icon="list" font-scale="2"></b-icon>
+                    <p>Amo a mi novia, consectetur adipisicing elit. Ab architecto consectetur cumque dignissimos eveniet id illo illum incidunt itaque, laudantium magnam minima nobis non qui quibusdam voluptatibus voluptatum. Dolores, eius.lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquam aliquid culpa dolorem eos et exercitationem fugiat perspiciatis praesentium quam quia quos repellendus reprehenderit saepe sequi tempora, temporibus voluptate voluptatibus?</p>
                 </b-col>
             </b-row>
         </b-container>
-        <SideBar/>
     </div>
 </template>
 
@@ -32,24 +17,17 @@
 
     export default {
         name: 'App',
-        data() {
-            return {
-                actualWidth: 350,
-                disp: true,
-                arrTabs: [true, false, false, false]
-            }
-        },
         components: {
             SideBar
         },
+        data(){
+            return {
+                disp: true
+            }
+        },
         methods: {
-            toggleNav(){
+            changeSidebarProp() {
                 this.disp = !this.disp;
-            },
-            colour(id){
-                let falseArr = this.arrTabs.map(() => false);
-                falseArr[id] = true;
-                this.arrTabs = falseArr;
             }
         }
     }
